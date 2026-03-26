70a703b8a23501b205d34dc105ed6a8c45f29819
Behnaz Abdul Razaq <behnazabdulrazaaq845@gmail.com>
2026-03-26 15:54:55 +0300

    mior demo changes in process

--- UsrYacht/Files/obj/Debug/UsrYacht.AssemblyInfo.cs
+++ UsrYacht/Files/obj/Debug/UsrYacht.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UsrYacht")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f8476ef715e20d1158fdf88e4e1ffcfa8950a94")]
 [assembly: System.Reflection.AssemblyProductAttribute("UsrYacht")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UsrYacht")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
